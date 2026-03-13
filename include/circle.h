2050bdc29868b62a387324f7c86f8591ae147800
unknown <lego.den.2005@mail.ru>
2026-03-13 16:17:13 +0300

    ST-2, branch b121: done

--- include/circle.h
+++ include/circle.h
@@ -5,25 +5,25 @@
 const double PI = 3.14159265358979323846;
 
 class Circle {
-  private:
-    double radius;
-    double ference;
-    double area;
+ private:
+  double radius;
+  double ference;
+  double area;
 
-    void updateFromRadius();
-    void updateFromFerence();
-    void updateFromArea();
+  void updateFromRadius();
+  void updateFromFerence();
+  void updateFromArea();
 
-  public:
-    explicit Circle(double r);
+ public:
+  explicit Circle(double r);
 
-    void setRadius(double r);
-    void setFerence(double f);
-    void setArea(double a);
+  void setRadius(double r);
+  void setFerence(double f);
+  void setArea(double a);
 
-    double getRadius() const;
-    double getFerence() const;
-    double getArea() const;
+  double getRadius() const;
+  double getFerence() const;
+  double getArea() const;
 };
 
 #endif // INCLUDE_CIRCLE_H_
